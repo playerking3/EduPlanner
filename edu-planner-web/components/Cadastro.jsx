f1d0a9e69493fe66d30e528a8b4115a4c6edf1f1
Ana Julia <124930475+najuvaroni@users.noreply.github.com>
2024-05-06 17:24:35 -0300

    comecei a página de cadastro

---
+++ edu-planner-web/components/Cadastro.jsx
@@ -0,0 +1,16 @@
+import React,{useState} from "react";
+import css from './Cadastro.module.css';
+function Cadastro() {
+    const [nome, setNome] = useState("");
+    const [senha, setSenha] = useState("");
+
+    return(
+        <div className={css.container}>
+            
+
+        </div>
+    )
+}
+
+
+export default Cadastro
