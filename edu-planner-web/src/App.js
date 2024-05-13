9138f2efff64af1417972b6dff747fc8878fea57
najuvaroni <124930475+najuvaroni@users.noreply.github.com>
2024-05-13 09:36:09 -0300

    realizei a página de edição

--- edu-planner-web/src/App.js
+++ edu-planner-web/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Cadastro from "./components/Cadastro";
+import Edicao from "./components/Edicao";
 
 function App() {
   return (
     <div className="App">
       <Cadastro></Cadastro>
+        <Edicao></Edicao>
 
     </div>
   );
