6c431b1b322d2ab44485015ac87f0bfb49194d2c
najuvaroni <124930475+najuvaroni@users.noreply.github.com>
2024-05-13 09:27:05 -0300

    finalizei css pág cadastro

--- edu-planner-web/src/components/Cadastro.jsx
+++ edu-planner-web/src/components/Cadastro.jsx
@@ -21,19 +21,61 @@ function Cadastro() {
 
             </div>
 
-            <div className={css.header}>
-                <div className={css.divtudo}>
-                    <h2 className={css.h2Cadastro}>Cadastrar novo usuário</h2>
 
-                    <div className={css.ptum}>
-                        <div className={css.nomeInp}><h3>Nome </h3><h3>*</h3></div>
-                        <input type="text" value={nome}
-                               onChange={(e) => setNome(e.target.value)} placeholder=" " id="nome"/>
+            <div className={css.header}>
+                <h2 className={css.h2Cadastro}>Edição de usuário</h2>
+
+                <div className={css.divtudo}>
+                    <div className={css.ladoEsquerdo}>
+                        <div className={css.ptum}>
+                            <div className={css.nomeInp}><h3>Nome </h3><h3 className={css.asterisco}>*</h3></div>
+                            <input type="text" value={nome}
+                                   onChange={(e) => setNome(e.target.value)} placeholder=" " id="nome"/>
+                        </div>
+                        <div className={css.ptdois}>
+                            <div className={css.nomeInp}><h3>Função </h3><h3 className={css.asterisco}>*</h3></div>
+                            <select className={css.select} id="cars">
+                                <option value="audi"></option>
+                                <option value="volvo">Coordenador</option>
+                                <option value="saab">Professor</option>
+                                <option value="mercedes">Aluno</option>
+
+                            </select>
+
+                        </div>
+                        <div className={css.pttres}>
+                            <div className={css.nomeInp}><h3>Escolher foto de perfil </h3></div>
+                            <button className={css.bttBaixar}><img className={css.imgBaixar} src='./baixar.png'/>
+                            </button>
+                        </div>
+                    </div>
+
+
+                    <div>
+                        <div className={css.ptquatro}>
+                            <div className={css.nomeInp}><h3>CPF </h3><h3 className={css.asterisco}>*</h3></div>
+                            <input type="text" value={nome}
+                                   onChange={(e) => setNome(e.target.value)} placeholder=" " id="nome"/>
+                        </div>
+                        <div className={css.ptcinco}>
+                            <div className={css.nomeInp}><h3>Data de nascimento </h3><h3 className={css.asterisco}>*</h3></div>
+                            <input type="text" value={nome}
+                                   onChange={(e) => setNome(e.target.value)} placeholder=" " id="nome"/>
+                        </div>
+                        <div className={css.ptseis}>
+                            <div className={css.nomeInp}><h3>E-mail </h3><h3 className={css.asterisco}>*</h3></div>
+                            <input type="text" value={nome}
+                                   onChange={(e) => setNome(e.target.value)} placeholder=" " id="nome"/>
+                        </div>
+                        <div className={css.ptsete}>
+                            <div className={css.nomeInp}><h3>Senha </h3><h3 className={css.asterisco}>*</h3></div>
+                            <input type="text" value={nome}
+                                   onChange={(e) => setNome(e.target.value)} placeholder=" " id="nome"/>
+                        </div>
                     </div>
                 </div>
-
+                <button className={css.bttSalvar}>Salvar</button>
             </div>
-
         </div>
     )
 }
