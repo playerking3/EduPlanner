bc6cdd5fbfd9229f8b3c80c492b2209666926c0b
playerking3 <123572431+playerking3@users.noreply.github.com>
2024-05-21 09:02:06 -0300

    home consertado

--- edu-planner-web/src/pages/Erro.jsx
+++ edu-planner-web/src/pages/Erro.jsx
@@ -1,18 +1,18 @@
-import './Erro.css';
+import css from './Erro.module.css';
 
 function Erro() {
     return (
-        <div className="amarelo">
-            <div className="brancao">
-                <img src={"logo.png"} className="logo" alt="Logo" />
-                <div className="informacoes">
-                    <div className="textos">
-                        <p className="num">404</p>
-                        <p className="p">Página não foi encontrada</p>
-                        <button className="button"> Voltar para home </button>
+        <div className={css.amarelo}>
+            <div className={css.brancao}>
+                <img src={"logo.png"} className={css.logo} alt="Logo" />
+                <div className={css.informacoes}>
+                    <div className={css.textos}>
+                        <p className={css.numero}>404</p>
+                        <p className={css.p}>Página não foi encontrada</p>
+                        <button className={css.botao}>Voltar para home</button>
                     </div>
-                    <div className="img">
-                        <img src={"cara.png"} className="cara" alt="Cara" />
+                    <div className={css.img}>
+                        <img src={"cara.png"} className={css.cara} alt="Cara" />
                     </div>
                 </div>
             </div>
