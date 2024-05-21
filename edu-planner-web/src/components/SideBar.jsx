1edd40c558de2286d5aa60667f12ce9132581d2a
Lucas Altino <124930759+LucasAltino@users.noreply.github.com>
2024-05-21 17:21:55 -0300

    Alteracoes styles

--- edu-planner-web/src/components/SideBar.jsx
+++ edu-planner-web/src/components/SideBar.jsx
@@ -9,7 +9,9 @@ function SideBar(){
             <div className={css.iconGroup}>
                 <Link to={'/cursos'}><i className={"fa-solid fa-book fa-xl " + css.icon}></i></Link>
                 <Link to={'/dashboard'}><i className={"fa-regular fa-calendar fa-xl " + css.icon}></i></Link>
-                <Link to={'/pessoas'}><i className={"fa-regular fa-user fa-xl " + css.icon}></i></Link>
+                <Link to={'/visualizacao-usuarios'}><i className={"fa-regular fa-user fa-xl " + css.icon}></i></Link>
+                <Link to={'/salas'} style={{fontWeight: 900,color: 'white', marginTop: '1.3em', fontSize:' 2vw'}}><i className={"fa-solid fa-house-user fa-xl" + css.icon}></i></Link>
+
             </div>
             <div>
                 <Link to={'/login'}><i className={"fa-solid fa-arrow-right-from-bracket "+ css.icon}></i></Link>
