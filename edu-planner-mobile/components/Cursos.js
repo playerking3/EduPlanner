d3e9adc87a3c9e04bce30d9beeeeac24e36c9307
LucasAltino <124930759+LucasAltino@users.noreply.github.com>
2024-06-17 17:22:08 -0300

    a

--- edu-planner-mobile/components/Cursos.js
+++ edu-planner-mobile/components/Cursos.js
@@ -1,75 +1,100 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView } from "react-native";
 import CardCurso from "./CardCurso";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export default function Home({ navigation }) {
+export default function VisualizacaoCurso({ navigation }) {
+  const [listaCursos, setListaCursos] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const token = await AsyncStorage.getItem('token');
+        const responseCursos = await fetch(props.api + '/getCursos', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ token })
+        });
+        const dataCursos = await responseCursos.json();
+        setListaCursos(dataCursos.cursos);
+
+        const responseTurmas = await fetch(props.api + '/getTurma', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ token })
+        });
+        const dataTurmas = await responseTurmas.json();
+        setTurmas(dataTurmas.lista_turma);
+      } catch (error) {
+        console.error('Erro ao carregar dados:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleDelete = async (id) => {
+    try {
+      setListaCursos(listaCursos.filter(curso => curso.id !== id));
+    } catch (error) {
+      console.error('Erro ao excluir curso:', error);
+    }
+  };
+
   const handleMenuPress = () => {
     navigation.openDrawer();
     console.log("Menu pressionado");
   };
 
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <TouchableOpacity onPress={handleMenuPress}>
-          <Image style={styles.hamburguer} source={require('../assets/hamburguer.png')} />
-        </TouchableOpacity>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity onPress={handleMenuPress}>
+            <Image style={styles.hamburguer} source={require('../assets/hamburguer.png')} />
+          </TouchableOpacity>
+        </View>
+
+        <ScrollView contentContainerStyle={styles.scrollContainer}>
+          {listaCursos.map((curso) => (
+              <CardCurso
+                  key={curso.id}
+                  id={curso.id}
+                  img={curso.imagem}
+                  titulo={curso.titulo}
+                  hora={curso.hora}
+                  curso={curso.curso}
+                  faixa={curso.faixa}
+                  descricao={curso.descricao}
+                  onDelete={handleDelete}
+              />
+          ))}
+        </ScrollView>
       </View>
-
-      <ScrollView contentContainerStyle={styles.scrollContainer}>
-        <CardCurso 
-          img={require("../assets/img1.png")}
-          titulo="Oficina de bolos"
-          hora="10"
-          descricao=""
-          curso='Culinária'
-          faixa='12 a 15'
-          descricao='Na Oficina de Bolos para Crianças, os participantes mergulham em uma experiência cheia de cores, sabores e muita diversão. Sob a orientação cuidadosa de nossos instrutores especializados, cada criança aprende os fundamentos da confeitaria enquanto deixa sua criatividade fluir livremente.
-Durante o curso, as crianças aprendem desde os conceitos básicos da preparação de massa até as técnicas de decoração mais elaboradas. Elas têm a oportunidade de misturar, mexer e decorar, criando verdadeiras obras-primas açucaradas com suas próprias mãos.'
-        />
-
-        <CardCurso 
-          img={require("../assets/img2.png")}
-          titulo="Dramaturgia"
-          hora="10"
-          descricao=""
-          curso='Teatro'
-           faixa='10 a 12'
-          descricao='Na Dramaturgia, os participantes são levados em uma jornada fascinante que os leva desde os conceitos básicos da construção de personagens até as técnicas avançadas de escrita de roteiros. Sob a orientação cuidadosa de nossos instrutores experientes, cada aluno é incentivado a explorar sua própria voz e visão artística, criando peças únicas e cativantes.
-
-Durante o curso, os alunos são desafiados a pensar profundamente sobre o mundo ao seu redor, a explorar questões importantes e a expressar suas ideias de maneira poderosa e impactante. Eles aprendem a arte de criar diálogos autênticos, a desenvolver tramas envolventes e a dar vida a personagens memoráveis que ressoam com o público.'
-        />
-
-        <CardCurso 
-          img={require("../assets/img3.png")}
-          titulo="Iniciação ao Inglês"
-          hora="40"
-          curso='Teatro'
-           faixa='8 a 14'
-          descricao="Um curso de inglês para iniciantes pode ser uma grande oportunidade de desbloquear conhecimento e iniciar novas habilidades. Falar outra língua pode parecer distante para quem nunca estudou, mas tudo começa com o primeiro passo. A Cultura Inglesa encara esse momento inicial como algo incrível. Por isso, os cursos para iniciantes ajudam pessoas a começarem uma jornada de aprendizado de maneira didática e interessante. É o começo da conquista de um objetivo. Se tem dúvidas sobre aulas de inglês para iniciantes e como funcionam, este conteúdo vai ajudar você."
-        />
-      </ScrollView>
-    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#FFDD26",
+    backgroundColor: '#fff',
   },
   header: {
-    backgroundColor: "#FFDD26",
-    height: 100,
-    justifyContent: 'center',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    padding: 10,
   },
   hamburguer: {
-    marginTop: 45,
-    marginLeft: 20,
+    width: 30,
+    height: 30,
   },
   scrollContainer: {
     flexGrow: 1,
-    alignItems: 'center',
-    paddingBottom: 20,
+    paddingVertical: 20,
+    paddingHorizontal: 10,
   },
 });
--- edu-planner-mobile/components/Perfil.js
+++ edu-planner-mobile/components/Perfil.js
@@ -1,8 +1,65 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity, Image, TextInput } from "react-native";
+import {View, Text, StyleSheet, TouchableOpacity, Image, TextInput, Pressable} from "react-native";
 
 export default function Perfil({ navigation }) {
   const [imageUri ] = React.useState(null);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const closeModal = () => {
+    setIsModalVisible(false);
+  };
+
+  async function pickImage(setImageUri, setPhoto) {
+    // Pedir permissão para acessar a biblioteca de fotos
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    // Verifica se a permissão não foi concedida
+    if (status !== 'granted') {
+      // Exibe uma mensagem de erro na tela
+      Alert.alert(
+          'Desculpe, precisamos de permissões da biblioteca de fotos para fazer isso funcionar!'
+      );
+      return; // Mata o processamento
+    }
+
+    // Permitir que o usuário selecione uma imagem da biblioteca
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    // Verifica se não deu erro
+    if (!result.canceled) {
+      // Passa a uri da imagem para o useState, para poder ser exibida
+      setPhoto(null);
+      setImageUri(result.assets[0].uri);
+    }
+  }
+
+  async function pickPhoto(setPhoto, setImageUri) {
+    // Pedindo permissão para acessar a câmera
+    const permissionResult = await ImagePicker.requestCameraPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      Alert.alert(
+          'Permissão necessária',
+          'Você precisa conceder permissão para acessar a câmera!'
+      );
+      return;
+    }
+
+    const result = await ImagePicker.launchCameraAsync({
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.cancelled) {
+      setImageUri(null);
+      setPhoto(result.assets[0].uri);
+    }
+  }
 
   const handleMenuPress = () => {
     navigation.openDrawer()
@@ -16,6 +73,33 @@ export default function Perfil({ navigation }) {
      </TouchableOpacity>
       <View style={styles.tudo}>
         <Image style={styles.lapis} source={require('../assets/lapis.png')} />
+        <Pressable
+            onPress={() => {
+              setIsModalVisible(true);
+            }}>
+          {!imageUri && !photo ? (
+              <Image
+                  source={require('../assets/lapis.png')}
+                  style={{
+                    width: 100,
+                    height: 100,
+                    marginTop: -10,
+                    marginLeft: 10,
+                    borderRadius: 10,
+                  }}
+              />
+          ) : (
+              <Image
+                  source={{ uri: imageUri ? imageUri : photo }}
+                  style={{
+                    width: 240,
+                    height: 200,
+                    marginTop: 10,
+                    borderRadius: 10,
+                  }}
+              />
+          )}
+        </Pressable>
         <Text style={styles.textoInp}>Nome de usuário</Text>
         <View style={styles.inputContainer}>
           <TextInput style={styles.input} placeholder="Nome" />
