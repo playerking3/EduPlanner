24addce988f69a4ebac14e9de346ead5d21a55d2
LucasAltino <124930759+LucasAltino@users.noreply.github.com>
2024-06-17 14:47:22 -0300

    correção mask cpf

--- edu-planner-web/src/components/BntSenhaOlho.jsx
+++ edu-planner-web/src/components/BntSenhaOlho.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import CadastroInput from "../components/CadastroInput";
 import css from './BntSenhaOlho.module.css';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 function BntSenhaOlho({ setSenha, senha }) {
     const [mostrarSenha, setMostrarSenha] = useState(false);
 
@@ -10,15 +11,12 @@ function BntSenhaOlho({ setSenha, senha }) {
             <div className={css.InputContainer}>
                 <CadastroInput
                     type={mostrarSenha ? 'text' : 'password'} name={'senha'} placeholder={'Senha'} setar={setSenha} valor={senha}/>
-                <button className={css.MudSenha} onClick={() => {setMostrarSenha(!mostrarSenha);}}>
-                    <img
-                        className={css.img}
-                        src={mostrarSenha ? './OlhoFechado.png' : './OlhoAberto.png'}
-                        alt={mostrarSenha ? 'Ocultar Senha' : 'Mostrar Senha'}
-                    />
+                <button className={css.MudSenha} onClick={() => setMostrarSenha(!mostrarSenha)}>
+                    <FontAwesomeIcon icon={mostrarSenha ?  faEye : faEyeSlash }/>
                 </button>
             </div>
         </div>
     );
 }
+
 export default BntSenhaOlho;
--- edu-planner-web/src/components/CadastroInput.jsx
+++ edu-planner-web/src/components/CadastroInput.jsx
@@ -1,24 +1,19 @@
 import css from './CadastroInput.module.css';
 import InputMask from 'react-input-mask';
 
-function CadastroInput({ name, type, placeholder, setar, valor, mask }) {
-    const alteracaoDeEntrada = (event) => {
-        setar(event.target.value);
-    };
 
-<<<<<<< HEAD
-=======
-function CadastroInput({ name, type, placeholder, setar, valor, desativo= false }) {
+function CadastroInput({ name, type, placeholder, setar, valor, mask }) {
     const handleChange = (event) => {
         const value = event.target.value;
         setar(value);
     }
->>>>>>> 7b1feae87377ecf3e39971c7cf424e5da8e99768
+    const alteracaoDeEntrada = (event) => {
+        setar(event.target.value);
+    };
     return (
         <div className={css.container}>
             <label htmlFor={name}>{placeholder} <span className={css.asterisco}>*</span> </label>
             <div className={css.inputGroup}>
-<<<<<<< HEAD
                 {mask ? (
                     <InputMask mask={mask} value={valor} onChange={alteracaoDeEntrada}>
                         {(inputProps) => <input {...inputProps} type={type} name={name} className={css.input} placeholder={placeholder} />}
@@ -26,9 +21,6 @@ function CadastroInput({ name, type, placeholder, setar, valor, desativo= false 
                 ) : (
                     <input type={type} name={name} className={css.input} placeholder={placeholder} onChange={alteracaoDeEntrada} value={valor}/>
                 )}
-=======
-                <input type={type} name={name} className={css.input} placeholder={placeholder} onChange={handleChange} value={valor} disabled={desativo}/>
->>>>>>> 7b1feae87377ecf3e39971c7cf424e5da8e99768
             </div>
         </div>
     );
